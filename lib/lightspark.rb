c33561fb3e0667fd1f7894d85916296289097c8d
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-02-26 21:36:55 -0800

    bundle gem

---
+++ lib/lightspark.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lightspark/version"
+
+module Lightspark
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lightspark/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Lightspark
+  VERSION = "0.1.0"
+end
